2f103822d7d1ac1b51dc63e053f93530225c992e
Paul Melinte <paul.melinte@softwire.com>
2021-06-30 12:52:54 +0300

    Split rover endpoint in another file

---
+++ src/rovers/roversRouter.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import axios from "axios";
+
+const apiKey = 'api_key=6xr9QIaOXNnYO0KLwLTfgUZSdH2gpuni7VOTAZYt';
+const apiUrlNASA = 'https://api.nasa.gov/mars-photos/api/v1';
+
+export const roversRouter = express.Router();
+
+roversRouter.get('/', (req, res) => {
+    const roversGetRes = axios.get(apiUrlNASA + '/rovers?' + apiKey,{})
+    .then(response => res.send(response.data))
+    .catch(err => console.log(err));
+});
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,15 @@
 import express from "express";
-import axios from "axios";
+import { roversRouter } from "./rovers/roversRouter";
 
 const app = express();
 const port = 8000;
-const apiKey = 'api_key=6xr9QIaOXNnYO0KLwLTfgUZSdH2gpuni7VOTAZYt';
-const apiUrlNASA = 'https://api.nasa.gov/mars-photos/api/v1';
 
 app.use(express.json());
+
 const router = express.Router();
+router.use('/rovers',roversRouter);
 
-router.get('/test', (req, res) => res.send('Hello world !'));
-
-router.get('/rovers', (req, res) => {
-    const roversGetRes = axios.get(apiUrlNASA + '/rovers?' + apiKey,{})
-    .then(response => res.send(response.data))
-    .catch(err => console.log(err));
-});
-
-app.use('/', router);
+app.use('/api', router);
  
 app.listen(port, () => {
   console.log(`Test backend is running on port ${port}`);
