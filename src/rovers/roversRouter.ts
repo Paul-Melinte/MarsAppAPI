4891291976e5c06d168e00c9f55dd8189359c46b
Paul Melinte <paul.melinte@softwire.com>
2021-06-30 15:22:16 +0300

    Trimmed response. TODO create function for this

--- src/rovers/roversRouter.ts
+++ src/rovers/roversRouter.ts
@@ -2,7 +2,7 @@ import express from "express";
 import axios from "axios";
 import { cameraTypes } from "./cameraTypes";
 import { apiKey, apiUrlNASA } from "../apiConsts";
- 
+import { TrimmedPhotoData } from "./photoData";
 
 export const roversRouter = express.Router();
 
@@ -20,9 +20,27 @@ roversRouter.get("/photos", (req, res) => {
         .catch(err => console.log(err));
 });
 
+
 // Endpoint hardcoded for sol
 roversRouter.get("/:roverNam/photos/:cameraName", (req, res) => {
     const photosGetRes = axios.get(apiUrlNASA + `/rovers/${req.params.roverNam}/photos?sol=100&camera=${req.params.cameraName}&` + apiKey)
-        .then(response => res.send(response.data))
+        .then(response => {
+            const photos: any[] = response.data.photos;
+            const trimmedPhotos: TrimmedPhotoData[] = [];
+
+            while(photos.length > 0) {
+                const photoData = photos.pop();
+                const trimmedPhotoData: TrimmedPhotoData = {
+                    id:photoData.id,
+                    img_url:photoData.img_src,
+                    sol:photoData.sol,
+                    earth_date:photoData.earth_date
+                };
+
+                trimmedPhotos.push(trimmedPhotoData);
+            }
+
+            res.send(trimmedPhotos);
+        })
         .catch(err => console.log(err));
 });
