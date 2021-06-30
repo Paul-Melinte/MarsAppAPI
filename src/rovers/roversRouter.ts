b1c0d052ac73ef1b9d11319582e15b950985cc00
Paul Melinte <paul.melinte@softwire.com>
2021-06-30 13:49:28 +0300

    Hardcoded photos endpoint

--- src/rovers/roversRouter.ts
+++ src/rovers/roversRouter.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import axios from "axios";
+import { cameraTypes } from "./cameraTypes";
 
 const apiKey = 'api_key=6xr9QIaOXNnYO0KLwLTfgUZSdH2gpuni7VOTAZYt';
 const apiUrlNASA = 'https://api.nasa.gov/mars-photos/api/v1';
@@ -12,3 +13,10 @@ roversRouter.get('/', (req, res) => {
         .then(response => res.send(response.data))
         .catch(err => console.log(err));
 });
+
+// Endpoint hardcoded for specific sol, rover, and camera
+roversRouter.get('/photos', (req, res) => {
+    const photosGetRes = axios.get(apiUrlNASA + '/rovers/curiosity/photos?sol=100&camera=FHAZ&' + apiKey)
+        .then(response => res.send(response.data))
+        .catch(err => console.log(err));
+})
