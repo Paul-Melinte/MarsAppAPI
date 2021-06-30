144ad25160038fb67ce5ecb405c0bce7a17be306
Paul Melinte <paul.melinte@softwire.com>
2021-06-30 16:19:56 +0300

    Fixed a typo

--- src/rovers/roversRouter.ts
+++ src/rovers/roversRouter.ts
@@ -21,8 +21,8 @@ roversRouter.get("/photos", (req, res) => {
 
 
 // Endpoint hardcoded for sol
-roversRouter.get("/:roverNam/photos/:cameraName", (req, res) => {
-    const photosGetRes = axios.get(apiUrlNASA + `/rovers/${req.params.roverNam}/photos?sol=100&camera=${req.params.cameraName}&` + apiKey)
+roversRouter.get("/:roverName/photos/:cameraName", (req, res) => {
+    const photosGetRes = axios.get(apiUrlNASA + `/rovers/${req.params.roverName}/photos?sol=100&camera=${req.params.cameraName}&` + apiKey)
         .then(response => res.send(trimPhotoData(response.data.photos)))
         .catch(err => console.log(err));
 });
