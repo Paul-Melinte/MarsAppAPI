99a369397499006c740e74dc0f56ef424b2c2d97
Paul Melinte <paul.melinte@softwire.com>
2021-06-30 12:40:21 +0300

    rovers get endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
 import express from "express";
- 
+import axios from "axios";
+
 const app = express();
 const port = 8000;
- 
+const apiKey = 'api_key=6xr9QIaOXNnYO0KLwLTfgUZSdH2gpuni7VOTAZYt';
+const apiUrlNASA = 'https://api.nasa.gov/mars-photos/api/v1';
+
 app.use(express.json());
 const router = express.Router();
+
 router.get('/test', (req, res) => res.send('Hello world !'));
+
+router.get('/rovers', (req, res) => {
+    const roversGetRes = axios.get(apiUrlNASA + '/rovers?' + apiKey,{})
+    .then(response => res.send(response.data))
+    .catch(err => console.log(err));
+});
+
 app.use('/', router);
  
 app.listen(port, () => {
