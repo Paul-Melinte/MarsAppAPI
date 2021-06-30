0f5c870c3c3f879552215e0749cd5a7a6829eb55
Paul Melinte <paul.melinte@softwire.com>
2021-06-30 13:52:43 +0300

    Moved NASA api constants to their file

--- src/rovers/roversRouter.ts
+++ src/rovers/roversRouter.ts
@@ -7,15 +7,15 @@ import { apiKey, apiUrlNASA } from "../apiConsts";
 export const roversRouter = express.Router();
 
 // Endpoint for rover data
-roversRouter.get('/', (req, res) => {
-    const roversGetRes = axios.get(apiUrlNASA + '/rovers?' + apiKey,{})
+roversRouter.get("/", (req, res) => {
+    const roversGetRes = axios.get(apiUrlNASA + "/rovers?" + apiKey,{})
         .then(response => res.send(response.data))
         .catch(err => console.log(err));
 });
 
 // Endpoint hardcoded for specific sol, rover, and camera
-roversRouter.get('/photos', (req, res) => {
-    const photosGetRes = axios.get(apiUrlNASA + '/rovers/curiosity/photos?sol=100&camera=FHAZ&' + apiKey)
+roversRouter.get("/photos", (req, res) => {
+    const photosGetRes = axios.get(apiUrlNASA + "/rovers/curiosity/photos?sol=100&camera=FHAZ&" + apiKey)
         .then(response => res.send(response.data))
         .catch(err => console.log(err));
 })
