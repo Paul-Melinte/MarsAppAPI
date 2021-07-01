71988efdd4f5c8c72dd29c0ae91ed184d9d06b5f
Paul Melinte <paul.melinte@softwire.com>
2021-07-01 09:17:26 +0300

    These should be all the single quotes this time. Renamed the trimmedPhotoData file

--- src/rovers/roversRouter.ts
+++ src/rovers/roversRouter.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import axios from "axios";
 import { apiKey, apiUrlNASA } from "../apiConsts";
-import { trimPhotoData } from "../photos/photoData";
+import { trimPhotoData } from "../photos/trimmedPhotoData";
+import { cameraTypes } from "../photos/cameraTypes";
 
 export const roversRouter = express.Router();
 
@@ -14,15 +15,15 @@ roversRouter.get("/", (req, res) => {
 
 // Endpoint hardcoded for specific sol, rover, and camera
 roversRouter.get("/photos", (req, res) => {
-    const photosGetRes = axios.get(apiUrlNASA + "/rovers/curiosity/photos?sol=100&camera=FHAZ&" + apiKey)
+    const photosGetRes = axios.get(apiUrlNASA + `/rovers/curiosity/photos?sol=100&camera=${cameraTypes.FHAZ}&` + apiKey)
         .then(response => res.send(trimPhotoData(response.data.photos)))
         .catch(err => console.log(err));
 });
 
 
 // Endpoint hardcoded for sol
-roversRouter.get("/:roverNam/photos/:cameraName", (req, res) => {
-    const photosGetRes = axios.get(apiUrlNASA + `/rovers/${req.params.roverNam}/photos?sol=100&camera=${req.params.cameraName}&` + apiKey)
+roversRouter.get("/:roverName/photos/:cameraName", (req, res) => {
+    const photosGetRes = axios.get(apiUrlNASA + `/rovers/${req.params.roverName}/photos?sol=100&camera=${req.params.cameraName}&` + apiKey)
         .then(response => res.send(trimPhotoData(response.data.photos)))
         .catch(err => console.log(err));
 });
