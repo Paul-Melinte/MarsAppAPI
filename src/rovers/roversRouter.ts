b63c937ccf423866ba33362867f35a2751258cd1
Paul Melinte <paul.melinte@softwire.com>
2021-07-02 10:44:03 +0300

    trimmed rove data to be used in the spa form

--- src/rovers/roversRouter.ts
+++ src/rovers/roversRouter.ts
@@ -3,14 +3,14 @@ import axios from "axios";
 import { apiKey, apiUrlNASA } from "../apiConsts";
 import { trimPhotoData } from "../photos/trimmedPhotoData";
 import { cameraTypes } from "../photos/cameraTypes";
-
+import { trimRoverData } from "./trimmedRoverData";
 
 export const roversRouter = express.Router();
 
 // Endpoint for rover data
 roversRouter.get("/", (req, res) => {
     const roversGetRes = axios.get(apiUrlNASA + "/rovers?" + apiKey,{})
-        .then(response => res.send(response.data))
+        .then(response => res.send(trimRoverData(response.data.rovers)))
         .catch(err => console.log(err));
 });
 
@@ -19,7 +19,7 @@ roversRouter.get("/photos", (req, res) => {
     const photosGetRes = axios.get(apiUrlNASA + `/rovers/curiosity/photos?sol=100&camera=${cameraTypes.FHAZ}&` + apiKey)
         .then(response => res.send(trimPhotoData(response.data.photos)))
         .catch(err => console.log(err));
-});
+});     
 
 
 // Endpoint hardcoded for sol
